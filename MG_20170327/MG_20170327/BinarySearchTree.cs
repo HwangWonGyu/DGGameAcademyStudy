8bb088197317253c9bf2838e995fae0068b7f934
HwangWonGyu <hwksdream@naver.com>
2017-03-27 16:10:31 +0900

    BinarySearchTree

---
+++ MG_20170327/MG_20170327/BinarySearchTree.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MG_20170327
+{
+	class BinarySearchTreeNode
+	{
+		public int val;
+		public BinarySearchTreeNode left;
+		public BinarySearchTreeNode right;
+
+		public BinarySearchTreeNode()
+		{
+			left = null;
+			right = null;
+		}
+
+		public BinarySearchTreeNode(int _val)
+		{
+			val = _val;
+			left = null;
+			right = null;
+		}
+	}
+
+	class BinarySearchTree
+	{
+		public BinarySearchTreeNode root;
+
+		public BinarySearchTree()
+		{
+			root = null;
+		}
+
+		public void Insert(int n)
+		{
+			BinarySearchTreeNode temp = new BinarySearchTreeNode(n);
+
+			if (root == null)
+			{
+				root = temp;
+			}
+			else // root != null
+			{
+				BinarySearchTreeNode search = root;
+				while (true)
+				{
+					if (temp.val == search.val)
+					{
+						Console.WriteLine("이미 있는 값입니다.");
+						break;
+					}
+					else if (temp.val < search.val)
+					{
+						if (search.left != null)
+							search = search.left;
+						else // search.left == null
+						{
+							search.left = temp;
+							break;
+						}
+					}
+					else if (search.val < temp.val)
+					{
+						if (search.right != null)
+							search = search.right;
+						else // search.right == null
+						{
+							search.right = temp;
+							break;
+						}
+					}
+				}
+			}
+		}
+
+		public void Delete(int n)
+		{
+			BinarySearchTreeNode deleteParent;
+			BinarySearchTreeNode maxChildParent;
+
+			// Search로 #1 '삭제할 위치'를 찾고
+			// 그 위치를 기준으로 왼쪽 자식들 중 가장 큰 자식을 올림
+			// 만약 왼쪽 자식들이 없다면 오른쪽 자식들 중 가장 작은 자식을 올림
+			// 자식을 올리려면 #2 '삭제할 위치의 부모의 위치'를 알아야 됨
+			// 이후 #3-1 '왼쪽 자식들 중 가장 큰 자식의 부모의 위치'를 알고 있어야 하거나
+			// 만약 왼쪽 자식들이 없다면 #3-2 '오른쪽 자식들 중 가장 작은 자식의 부모의 위치'를 알아야 됨
+
+		}
+
+		public void Search(int n)
+		{
+			if (root == null)
+			{
+				Console.WriteLine("못찾았습니다.");
+			}
+			else // root != null
+			{
+				BinarySearchTreeNode search = root;
+				while (true)
+				{
+					if (n == search.val)
+					{
+						Console.WriteLine("찾았습니다.");
+						break;
+					}
+					else if (n < search.val)
+					{
+						if (search.left != null)
+							search = search.left;
+						else // search.left == null
+						{
+							Console.WriteLine("못찾았습니다.");
+							break;
+						}
+					}
+					else if (search.val < n)
+					{
+						if (search.right != null)
+							search = search.right;
+						else // search.right == null
+						{
+							Console.WriteLine("못찾았습니다.");
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ MG_20170327/MG_20170327/MainProgram.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MG_20170327
+{
+	class MainProgram
+	{
+		public static void ArrayBinarySearchIterative(int[] arr, int search, int start, int end, int divide)
+		{
+			bool isSearched = false;
+			while (start != end)
+			{
+				if (arr[divide] == search)
+				{
+					isSearched = true;
+					break;
+				}
+				else if (arr[divide] > search)
+				{
+					end = (start + end) / 2;
+					divide = (start + end) / 2;
+					Console.WriteLine("start = {0}, end = {1}, divide = {2}", start, end, divide);
+				}
+				else if (arr[divide] < search)
+				{
+					start = divide + 1;
+					divide = (start + end) / 2;
+					Console.WriteLine("start = {0}, end = {1}, divide = {2}", start, end, divide);
+				}
+			}
+			if (isSearched == true)
+				Console.WriteLine("찾았습니다.");
+			else
+				Console.WriteLine("못찾았습니다.");
+		}
+
+		public static void ArrayBinarySearchRecursive(int[] arr, int search, int start, int end, int divide)
+		{
+			if (start == end)
+			{
+				if (start < arr.Length && arr[start] == search)
+				{
+					Console.WriteLine("찾았습니다.");
+					return;
+				}
+				else
+				{
+					Console.WriteLine("못찾았습니다.");
+					return;
+				}
+			}
+
+			if (arr[divide] == search)
+			{
+				Console.WriteLine("찾았습니다.");
+				return;
+			}
+			else if (arr[divide] > search)
+			{
+				end = (start + end) / 2;
+				divide = (start + end) / 2;
+				Console.WriteLine("start = {0}, end = {1}, divide = {2}", start, end, divide);
+				ArrayBinarySearchRecursive(arr, search, start, end, divide);
+			}
+			else if (arr[divide] < search)
+			{
+				start = divide + 1;
+				divide = (start + end) / 2;
+				Console.WriteLine("start = {0}, end = {1}, divide = {2}", start, end, divide);
+				ArrayBinarySearchRecursive(arr, search, start, end, divide);
+			}
+		}
+
+		static void Main(string[] args)
+		{
+			//int[] arr = new int[15];
+			//for (int i = 0; i < 15; i++)
+			//	arr[i] = i * 2; // {0, 2, 4, ... ,26, 28}
+
+			//int start = 0;
+			//int end = arr.Length; // 15
+			//int divide = (start + end) / 2; // (0 + 15) / 2 = 7
+
+			//Console.Write("배열에서 찾아낼 정수를 입력하세요. : ");
+			//int search = Convert.ToInt32(Console.ReadLine());
+
+			//Console.WriteLine("start = {0}, end = {1}, divide = {2}", start, end, divide);
+
+			////iterative
+			//ArrayBinarySearchIterative(arr, search, start, end, divide);
+
+			////recursive
+			//ArrayBinarySearchRecursive(arr, search, start, end, divide);
+
+
+
+			BinarySearchTree bst = new BinarySearchTree();
+
+			bst.Insert(15);
+			bst.Insert(13);
+			bst.Insert(14);
+			bst.Insert(9);
+			bst.Insert(19);
+			bst.Insert(21);
+			bst.Insert(17);
+			bst.Insert(11);
+			bst.Insert(8);
+			bst.Insert(16);
+			bst.Insert(23);
+			bst.Insert(20);
+
+			bst.Search(21); // 찾았습니다.
+			bst.Search(15); // 찾았습니다.
+			bst.Search(14); // 찾았습니다.
+			bst.Search(11); // 찾았습니다.
+
+			bst.Search(18); // 못찾았습니다.
+			bst.Insert(18);
+			bst.Search(18); // 찾았습니다.
+			bst.Insert(18); // 이미 있는 값입니다.
+		}
+	}
+}
